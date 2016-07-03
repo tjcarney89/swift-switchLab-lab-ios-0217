0343d01dfeba711575516f2347a6e60cf302df0a
Guy Ardito <Guy@Guys-MBP.home>
2016-07-02 20:23:13 -0400

    doing again b/c last time not getting recognized

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -20,17 +20,10 @@ let b = 32
 
 
 /*: Question 1
-### 1. Print "true" if a modulo b is zero
+### 1. Print "true" if a modulo b is equal to zero
 */
 // write your code here
-switch a % b {
-case 0:
-	print("true")
-	
-default:
-	// do nothing
-	break
-}
+
 
 
 
@@ -39,13 +32,7 @@ default:
 ### 2. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
-switch Double(y) / x > Double(3) {
-case true:
-	print("true")
-	
-case false:
-	print("false")
-}
+
 
 
 
@@ -54,12 +41,7 @@ case false:
 ### 3. Print "true" if y is greater than x and a divided by b is grater than 9
 */
 // write your code here
-switch Double(y) > x  &&  a / b > 9 {
-case true:
-	print("true")
-case false:
-	print("false")
-}
+
 
 
 
@@ -67,14 +49,7 @@ case false:
 ### 4. Write a function "isGreater" that takes two Int arguments and returns "yep" if a is greater than b and "nope" if they're not
 */
 // write your code here
-func isGreater(a:Int, b:Int) -> String {
-	switch a > b {
-	case true:
-		return "yep"
-	case false:
-		return "nope"
-	}
-}
+
 
 
 
@@ -84,53 +59,14 @@ func isGreater(a:Int, b:Int) -> String {
 ### 5. write a function "isForceWith" that takes a String argument and returns true if the argument is the name of some whom the force is strong with, otherwise it returns false
 */
 // write your code here
-func isForceWith(name:String) -> Bool {
-	switch name {
-		
-	case "Luke":
-		return true
-		
-	case "Leia":
-		return true
-		
-	case "Anakin":
-		return true
-		
-	case "Obi Wan":
-		return true
-		
-	case "Yoda":
-		return true
-		
-	case "Vader":
-		return true
-		
-	default:
-		return false
-	}
-}
+
 
 
 /*: Question 6
 ### 6. write a function "isInRange" that takes an Int  argument and prints "small" if it's in the range of 0 and 3, "medium" if it's in the range of 4 and 6, "big" if it's in the range of 7 and 10, and otherwise prints "not sure".
 */
 
-func isInRange(a:Int) {
-	switch a {
-		
-	case 0...3:
-		print("small")
-		
-	case 4...6:
-		print("medium")
-		
-	case 7...10:
-		print("big")
-		
-	default:
-		print("not sure")
-	}
-}
+
 
 
 
