99e29518865fab54c5c7bb136637659464d2aa7a
JimCampagno <jimcampagno@gmail.com>
2016-07-06 10:07:34 -0400

    remove solution

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -71,6 +71,6 @@ let b = 32
 
 
 /*:
- [Solution](solution)
+ Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
  */
 // ❤️
